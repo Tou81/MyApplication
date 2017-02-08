118cc740f508ac1535e04a2670b1ea797e94b1b4
Tou81 <960290915@qq.com>
2017-02-08 15:31:03 +0800

    测试

---
+++ app/src/main/java/com/zc/myapplication/TestActivity.java
@@ -0,0 +1,32 @@
+package com.zc.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by dingbin on 2017/2/8.
+ */
+
+public class TestActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
